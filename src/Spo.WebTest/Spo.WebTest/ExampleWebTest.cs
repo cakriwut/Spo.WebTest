ceaed11c9da01489cd4d71a48965a7adcb2239f6
Riwut Libinuko <cakriwut@gmail.com>
2017-06-15 16:53:51 +0800

    Complete login. Allow handling login using ADFS or Microsoft Live.

---
+++ src/Spo.WebTest/Spo.WebTest/ExampleWebTest.cs
@@ -0,0 +1,112 @@
+﻿namespace Spo.WebTest
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.WebTesting;
+    using Microsoft.VisualStudio.TestTools.WebTesting.Rules;
+
+    [IncludeCodedWebTest("Spo.WebTest.SpoLogin", @"SpoLogin.cs")]
+    public class ExampleWebTest : WebTest
+    {
+        
+
+        public ExampleWebTest()
+        {
+            this.Context.Add("Tenant", "");  
+            this.Context.Add("UserName", "");
+            this.Context.Add("UserPassword", "");
+            this.PreAuthenticate = true;
+            this.Proxy = "default";
+        }
+
+        public override IEnumerator<WebTestRequest> GetRequestEnumerator()
+        {
+            // Initialize validation rules that apply to all requests in the WebTest
+            if ((this.Context.ValidationLevel >= Microsoft.VisualStudio.TestTools.WebTesting.ValidationLevel.Low))
+            {
+                ValidateResponseUrl validationRule1 = new ValidateResponseUrl();
+                this.ValidateResponse += new EventHandler<ValidationEventArgs>(validationRule1.Validate);
+            }
+            if ((this.Context.ValidationLevel >= Microsoft.VisualStudio.TestTools.WebTesting.ValidationLevel.Low))
+            {
+                ValidationRuleResponseTimeGoal validationRule2 = new ValidationRuleResponseTimeGoal();
+                validationRule2.Tolerance = 0D;
+                this.ValidateResponseOnPageComplete += new EventHandler<ValidationEventArgs>(validationRule2.Validate);
+            }
+
+            
+            //
+            foreach (WebTestRequest r in IncludeWebTest("SpoLogin", false)) { yield return r; };
+
+            WebTestRequest request1 = new WebTestRequest($"{this.Context["Tenant"].ToString()}/_vti_bin/client.svc/ProcessQuery");
+            request1.Method = "POST";
+            request1.Headers.Add(new WebTestRequestHeader("X-Requested-With", "XMLHttpRequest"));
+            request1.Headers.Add(new WebTestRequestHeader("X-RequestDigest", this.Context["$HIDDENCtx.__REQUESTDIGEST"].ToString()));
+            StringHttpBody request1Body = new StringHttpBody();
+            request1Body.ContentType = "text/xml";
+            request1Body.InsertByteOrderMark = false;
+            request1Body.BodyString = "<Request xmlns=\"http://schemas.microsoft.com/sharepoint/clientquery/2009\" SchemaV" +
+                "ersion=\"15.0.0.0\" LibraryVersion=\"16.0.0.0\" ApplicationName=\"Javascript Library\"" +
+                "><Actions><ObjectPath Id=\"1\" ObjectPathId=\"0\" /><ObjectPath Id=\"3\" ObjectPathId=" +
+                "\"2\" /><ObjectPath Id=\"5\" ObjectPathId=\"4\" /><ObjectPath Id=\"7\" ObjectPathId=\"6\" " +
+                "/><ObjectPath Id=\"9\" ObjectPathId=\"8\" /><ExceptionHandlingScopeSimple Id=\"10\"><M" +
+                "ethod Name=\"ValidateUpdateListItem\" Id=\"12\" ObjectPathId=\"8\"><Parameters><Parame" +
+                "ter Type=\"Array\"><Object TypeId=\"{03745a5a-2400-440e-92d6-dad4afee30a6}\"><Proper" +
+                "ty Name=\"ErrorMessage\" Type=\"Null\" /><Property Name=\"FieldName\" Type=\"String\">Ti" +
+                "tle</Property><Property Name=\"FieldValue\" Type=\"String\">Test Hello</Property><Pr" +
+                "operty Name=\"HasException\" Type=\"Boolean\">false</Property></Object><Object TypeI" +
+                "d=\"{03745a5a-2400-440e-92d6-dad4afee30a6}\"><Property Name=\"ErrorMessage\" Type=\"N" +
+                "ull\" /><Property Name=\"FieldName\" Type=\"String\">StartDate</Property><Property Na" +
+                "me=\"FieldValue\" Type=\"String\">6/14/2017</Property><Property Name=\"HasException\" " +
+                "Type=\"Boolean\">false</Property></Object><Object TypeId=\"{03745a5a-2400-440e-92d6" +
+                "-dad4afee30a6}\"><Property Name=\"ErrorMessage\" Type=\"Null\" /><Property Name=\"Fiel" +
+                "dName\" Type=\"String\">DueDate</Property><Property Name=\"FieldValue\" Type=\"String\"" +
+                "></Property><Property Name=\"HasException\" Type=\"Boolean\">false</Property></Objec" +
+                "t><Object TypeId=\"{03745a5a-2400-440e-92d6-dad4afee30a6}\"><Property Name=\"ErrorM" +
+                "essage\" Type=\"Null\" /><Property Name=\"FieldName\" Type=\"String\">PercentComplete</" +
+                "Property><Property Name=\"FieldValue\" Type=\"String\"></Property><Property Name=\"Ha" +
+                "sException\" Type=\"Boolean\">false</Property></Object><Object TypeId=\"{03745a5a-24" +
+                "00-440e-92d6-dad4afee30a6}\"><Property Name=\"ErrorMessage\" Type=\"Null\" /><Propert" +
+                "y Name=\"FieldName\" Type=\"String\">Body</Property><Property Name=\"FieldValue\" Type" +
+                "=\"String\">&lt;div&gt;&lt;/div&gt;</Property><Property Name=\"HasException\" Type=\"" +
+                "Boolean\">false</Property></Object><Object TypeId=\"{03745a5a-2400-440e-92d6-dad4a" +
+                "fee30a6}\"><Property Name=\"ErrorMessage\" Type=\"Null\" /><Property Name=\"FieldName\"" +
+                " Type=\"String\">Predecessors</Property><Property Name=\"FieldValue\" Type=\"String\">" +
+                "</Property><Property Name=\"HasException\" Type=\"Boolean\">false</Property></Object" +
+                "><Object TypeId=\"{03745a5a-2400-440e-92d6-dad4afee30a6}\"><Property Name=\"ErrorMe" +
+                "ssage\" Type=\"Null\" /><Property Name=\"FieldName\" Type=\"String\">Priority</Property" +
+                "><Property Name=\"FieldValue\" Type=\"String\">(2) Normal</Property><Property Name=\"" +
+                "HasException\" Type=\"Boolean\">false</Property></Object><Object TypeId=\"{03745a5a-" +
+                "2400-440e-92d6-dad4afee30a6}\"><Property Name=\"ErrorMessage\" Type=\"Null\" /><Prope" +
+                "rty Name=\"FieldName\" Type=\"String\">Status</Property><Property Name=\"FieldValue\" " +
+                "Type=\"String\">Not Started</Property><Property Name=\"HasException\" Type=\"Boolean\"" +
+                ">false</Property></Object><Object TypeId=\"{03745a5a-2400-440e-92d6-dad4afee30a6}" +
+                "\"><Property Name=\"ErrorMessage\" Type=\"Null\" /><Property Name=\"FieldName\" Type=\"S" +
+                "tring\">AssignedTo</Property><Property Name=\"FieldValue\" Type=\"String\"></Property" +
+                "><Property Name=\"HasException\" Type=\"Boolean\">false</Property></Object><Object T" +
+                "ypeId=\"{03745a5a-2400-440e-92d6-dad4afee30a6}\"><Property Name=\"ErrorMessage\" Typ" +
+                "e=\"Null\" /><Property Name=\"FieldName\" Type=\"String\">ContentType</Property><Prope" +
+                "rty Name=\"FieldValue\" Type=\"String\">Task</Property><Property Name=\"HasException\"" +
+                " Type=\"Boolean\">false</Property></Object><Object TypeId=\"{03745a5a-2400-440e-92d" +
+                "6-dad4afee30a6}\"><Property Name=\"ErrorMessage\" Type=\"Null\" /><Property Name=\"Fie" +
+                "ldName\" Type=\"String\">ContentTypeId</Property><Property Name=\"FieldValue\" Type=\"" +
+                "String\">0x010800DB18DF543E2EED45AD5C1065CD17430A</Property><Property Name=\"HasEx" +
+                "ception\" Type=\"Boolean\">false</Property></Object></Parameter><Parameter Type=\"Bo" +
+                "olean\">false</Parameter><Parameter Type=\"Null\" /></Parameters></Method></Excepti" +
+                "onHandlingScopeSimple></Actions><ObjectPaths><StaticProperty Id=\"0\" TypeId=\"{374" +
+                "7adcd-a3c3-41b9-bfab-4a64dd2f1e0a}\" Name=\"Current\" /><Property Id=\"2\" ParentId=\"" +
+                "0\" Name=\"Web\" /><Property Id=\"4\" ParentId=\"2\" Name=\"Lists\" /><Method Id=\"6\" Pare" +
+                "ntId=\"4\" Name=\"GetById\"><Parameters><Parameter Type=\"String\">40ec8848-453b-47eb-" +
+                "95ab-7bc0cc4ca9b0</Parameter></Parameters></Method><Method Id=\"8\" ParentId=\"6\" N" +
+                "ame=\"AddItem\"><Parameters><Parameter TypeId=\"{54cdbee5-0897-44ac-829f-411557fa11" +
+                "be}\"><Property Name=\"FolderUrl\" Type=\"String\">/Lists/Tasks</Property><Property N" +
+                "ame=\"LeafName\" Type=\"Null\" /><Property Name=\"UnderlyingObjectType\" Type=\"Number\"" +
+                ">0</Property></Parameter></Parameters></Method></ObjectPaths></Request>";
+            request1.Body = request1Body;
+            yield return request1;
+            request1 = null;
+        }
+
+        
+    }
+}
---
+++ src/Spo.WebTest/Spo.WebTest/SpoLogin.cs
@@ -0,0 +1,231 @@
+﻿namespace Spo.WebTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.WebTesting;
+    using Microsoft.VisualStudio.TestTools.WebTesting.Rules;
+    using OpenQA.Selenium.Remote;
+    using OpenQA.Selenium.Support.UI;
+    using OpenQA.Selenium.PhantomJS;
+    using System.Text.RegularExpressions;
+    using PCLWebUtility;
+    using OpenQA.Selenium;
+    using System.Reflection;
+    using System.IO;
+
+    [DeploymentItem("phantomjs.exe")]
+    public class SpoLogin : WebTest
+    {
+        
+        private RemoteWebDriver driver;
+        private WebDriverWait wait { get; set; }
+        private TimeSpan timeout;
+
+        public SpoLogin()
+        {
+            timeout = TimeSpan.FromSeconds(30);
+        }
+
+        public override IEnumerator<WebTestRequest> GetRequestEnumerator()
+        {
+            this.AddCommentToResult("Browse the homepage");
+            // Open the homepage
+            WebTestRequest request1 = new WebTestRequest(("https://"
+                            + (this.Context["Tenant"].ToString() + ".sharepoint.com")));
+            request1.FollowRedirects = false;
+            ExtractText extractionRule1 = new ExtractText();
+            extractionRule1.StartsWith = "href=\"";
+            extractionRule1.EndsWith = "\">";
+            extractionRule1.IgnoreCase = false;
+            extractionRule1.UseRegularExpression = false;
+            extractionRule1.Required = true;
+            extractionRule1.ExtractRandomMatch = false;
+            extractionRule1.Index = 0;
+            extractionRule1.HtmlDecode = true;
+            extractionRule1.SearchInHeaders = false;
+            extractionRule1.ContextParameterName = "Authenticate302";
+            request1.ExtractValues += new EventHandler<ExtractionEventArgs>(extractionRule1.Extract);
+            yield return request1;
+            request1 = null;
+
+            WebTestRequest request2 = new WebTestRequest(this.Context["Authenticate302"].ToString());
+            request2.FollowRedirects = false;
+            request2.Headers.Add(new WebTestRequestHeader("referer", ("https://"
+                                + (this.Context["Tenant"].ToString() + ".sharepoint.com"))));
+            ExtractText extractionRule2 = new ExtractText();
+            extractionRule2.StartsWith = "href=\"";
+            extractionRule2.EndsWith = "\">";
+            extractionRule2.IgnoreCase = false;
+            extractionRule2.UseRegularExpression = false;
+            extractionRule2.Required = true;
+            extractionRule2.ExtractRandomMatch = false;
+            extractionRule2.Index = 0;
+            extractionRule2.HtmlDecode = true;
+            extractionRule2.SearchInHeaders = false;
+            extractionRule2.ContextParameterName = "AuthSpForms";
+            request2.ExtractValues += new EventHandler<ExtractionEventArgs>(extractionRule2.Extract);
+            yield return request2;
+            request2 = null;
+
+            this.AddCommentToResult("Authentication starts");
+
+            //foreach (WebTestRequest r in IncludeWebTest("SpoLogin", false)) { yield return r; };
+            driver = new PhantomJSDriver(AssemblyDirectory);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            var baseURl = $"https://{this.Context["Tenant"].ToString()}.sharepoint.com";
+            string pattern = @"(?<=CONTEXT \= \').*(?=\';)";
+
+            driver.Navigate().GoToUrl(baseURl);            
+            var match = Regex.Match(driver.PageSource, pattern);
+
+            var validatorURl = $"https://login.microsoftonline.com/common/userrealm?user={this.Context["UserName"].ToString()}&api-version=2.1&stsRequest={match.Value}&checkForMicrosoftAccount=true";
+
+            driver.Navigate().GoToUrl(validatorURl);
+
+            pattern = "(?<=AuthURL\\\"\\:\\\")[^\\\"]*(?=\\\")";
+            match = Regex.Match(driver.PageSource, pattern);
+            if (match.Success)
+            {
+                FederatedLogin(WebUtility.HtmlDecode(match.Value), this.Context["UserName"].ToString(), this.Context["UserPassword"].ToString());
+            }
+            else
+            {
+                AzureDomainLogin(baseURl, this.Context["UserName"].ToString(), this.Context["UserPassword"].ToString());
+            }
+            // Get Hidden
+            var content = driver.PageSource;
+            var document = new HtmlAgilityPack.HtmlDocument();
+            document.LoadHtml(content);
+            foreach (var hidden in document.DocumentNode.Descendants("input")
+                            .Where(u => u.GetAttributeValue("type", "") == "hidden"))
+            {
+                var attributeName = hidden.GetAttributeValue("name", "");
+                var attributeValue = hidden.GetAttributeValue("value", "");
+
+                if (!String.IsNullOrEmpty(attributeValue))
+                {
+                    var hiddenContextName = $"$HIDDENCtx.{attributeName}";
+                    this.Context[hiddenContextName] = attributeValue;
+                }
+            }
+
+            driver.Quit();
+
+            // Check if request digest is empty, need to post token to SharePoint
+            if (this.Context["$HIDDENCtx.__REQUESTDIGEST"] == null)
+            {
+                WebTestRequest request8 = new WebTestRequest(("https://"
+                    + (this.Context["Tenant"].ToString()
+                    + (".sharepoint.com" + this.Context["AuthSpForms"].ToString()))));
+                request8.Method = "POST";
+                request8.ExpectedResponseUrl = ("https://"
+                            + (this.Context["Tenant"].ToString() + ".sharepoint.com"));
+                request8.Headers.Add(new WebTestRequestHeader("Referer", (this.Context["BaseMsoUrl"].ToString() + this.Context["LoginMsoPost"].ToString())));
+                FormPostHttpBody request8Body = new FormPostHttpBody();
+                request8Body.FormPostParameters.Add("code", this.Context["$HIDDENLogin.code"].ToString());
+                request8Body.FormPostParameters.Add("id_token", this.Context["$HIDDENLogin.id_token"].ToString());
+                request8Body.FormPostParameters.Add("state", this.Context["$HIDDENLogin.state"].ToString());
+                request8Body.FormPostParameters.Add("session_state", this.Context["$HIDDENLogin.session_state"].ToString());
+                request8.Body = request8Body;
+                ExtractHiddenFields extractionRule9 = new ExtractHiddenFields();
+                extractionRule9.Required = true;
+                extractionRule9.HtmlDecode = true;
+                extractionRule9.ContextParameterName = "Ctx";
+                request8.ExtractValues += new EventHandler<ExtractionEventArgs>(extractionRule9.Extract);
+                yield return request8;
+                request8 = null;
+            }
+
+
+
+            this.AddCommentToResult("Authentication completed");
+            
+        }
+         
+
+        private void AzureDomainLogin(string baseURl, string userName, string password)
+        {
+            driver.Navigate().GoToUrl(baseURl);
+            driver.FindElementById("cred_userid_inputtext").SendKeys(userName);
+            driver.FindElementById("cred_password_inputtext").SendKeys(password);
+            driver.FindElementById("cred_sign_in_button").Click();
+            var wait = new WebDriverWait(driver, timeout);
+            wait.Until(driver =>
+            {
+                bool isCompleted = (bool)((IJavaScriptExecutor)driver)
+                    .ExecuteScript("return $('#redirect_dots_animation').css('visibility') == 'hidden'");
+
+                return isCompleted;
+            });
+
+            driver.FindElementById("cred_sign_in_button").Click();
+            wait.Until(driver =>
+            {
+                bool isCompleted = (bool)((IJavaScriptExecutor)driver)
+                  .ExecuteScript("return $('#s4-ribbonrow').length != 0");
+
+                bool isLoaded = (bool)((IJavaScriptExecutor)driver)
+                  .ExecuteScript("return $('#O365_MeFlexPane_ButtonID').length != 0");
+
+                return isCompleted && isLoaded;
+            });
+        }
+
+        private string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        private void FederatedLogin(string loginURl, string userName, string password)
+        {
+            var listNameValue = new Dictionary<string, string>();
+
+            driver.Navigate().GoToUrl(loginURl);
+            var content = driver.PageSource;
+            var document = new HtmlAgilityPack.HtmlDocument();
+            document.LoadHtml(content);
+
+            var postUrl = WebUtility.HtmlDecode(document.DocumentNode.Descendants("form")
+                            .Where(u => u.GetAttributeValue("name", "") == "f1")
+                            .SingleOrDefault().GetAttributeValue("action", ""));
+
+            foreach (var hidden in document.DocumentNode.Descendants("input")
+                            .Where(u => u.GetAttributeValue("type", "") == "hidden"))
+            {
+                var attributeName = hidden.GetAttributeValue("name", "");
+                var attributeValue = hidden.GetAttributeValue("value", "");
+
+                if (!String.IsNullOrEmpty(attributeValue))
+                {
+                    var hiddenContextName = $"$HIDDEN1.{attributeName}";
+                    listNameValue[hiddenContextName] = attributeValue;
+                }
+            }
+
+
+            foreach (var hidden in document.DocumentNode.Descendants("input")
+                            .Where(u => u.GetAttributeValue("type", "") != "hidden"))
+            {
+                var attributeName = hidden.GetAttributeValue("name", "");
+                var attributeValue = hidden.GetAttributeValue("value", "");
+
+                if (!String.IsNullOrEmpty(attributeValue))
+                {
+                    var hiddenContextName = $"$INPUT1.{attributeName}";
+                    listNameValue[hiddenContextName] = attributeValue;
+                }
+            }
+
+            driver.FindElementByName("passwd").SendKeys(password);
+            driver.FindElementById("idSIButton9").Click();
+        }
+    }
+}
